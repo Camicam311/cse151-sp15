3fa5969abbe0e72be62dbc2dcec1d51992ac66ef
Chris Weller <chriskweller@gmail.com>
2015-05-28 20:56:18 -0700

    IT LIIIIIIIIIIVES!

--- hw5/perceptron.py
+++ hw5/perceptron.py
@@ -1,16 +1,15 @@
 from tabulate import tabulate
-from numpy import dot
 import numpy
 
-data_training_a = [numpy.array(map(int,x.rstrip().split())) for x in 
+data_training_a = [numpy.array(map(str,x.rstrip().split())) for x in 
         open("hw5train.txt").read().splitlines()]
 
-data_testing_a = [numpy.array(map(int,x.rstrip().split())) for x in 
+data_testing_a = [numpy.array(map(str,x.rstrip().split())) for x in 
         open("hw5test.txt").read().splitlines()]
 
-class wc:
-    w = []
-    c = 1
+def build_substring_matrix(data, length):
+    return [[num_common_substr(data[x][0],data[y][0],length) for y in 
+      xrange(len(data))] for x in xrange(len(data))]
 
 def num_common_substr(str_1, str_2, length):
     sub_list = set()
@@ -20,31 +19,26 @@ def num_common_substr(str_1, str_2, length):
 
     return len(sub_list)
 
-def process_label(label, classi):
-    return 1 if label == classi else -1
-
-def build_basic_perceptron(data, substr_len, sought_class=0):
-    w = 0
-    for feature in data:
-
-        # What is my second string for searching for common substrings?
-        if int(process_label(feature[-1],sought_class) * numpy.sum(dot(w, 
-          num_common_substr(feature[0:len(feature)-1], None, substr_len))))<=0:
-
-            # What is my second string for searching for common substrings?
-            w = w + dot(process_label(feature[-1],sought_class),
-              num_common_substr(feature[0:len(feature)-1], None, substr_len))
+def build_basic_perceptron(data, substr_len, sim_matrix, sought_class=1):
+    w = [(0,0,0)]
+    for idx, (protien, classification) in enumerate(data):
+        dotio = reduce(lambda acc,(pro,clas,row):acc+clas*sim_matrix[row][idx],w,0)
+        if int(classification) * dotio <= 0:
+            w += [(protien, int(classification), idx)]
     return w
 
-def classify_basic_perceptron(datum, classification_vector):
-    return numpy.sign(numpy.dot(datum, classification_vector))
+def classify_basic_perceptron(datum, train_data, w, k):
+    x = reduce(lambda acc,(pro,clas,row):
+      acc+clas*num_common_substr(data_training_a[row][0],datum, k),w,0)
 
-def classify_perceptron_set(data, classifier, function, sought_class=1):
+    return numpy.sign(x)
+
+def classify_perceptron_set(data, classifier, function, num, sought_class=1):
     error_count = 0
 
     for feature in data:
-        res = function(feature[:len(feature)-1], classifier)
-        if res != process_label(feature[-1],sought_class):
+        res = function(feature[0],data,classifier, num)
+        if res != int(feature[-1]):
             error_count += 1
 
     return str((error_count/float(len(data))) * 100) + "%"
@@ -59,17 +53,18 @@ def perform_tests(d_train, d_test):
     for idx,num in enumerate([3, 4]):
         score_table[idx + 1][0] = str(num)
 
-        bp = build_basic_perceptron(d_train, num)
+        sim_matrix = build_substring_matrix(d_train, num)
+
+        bp = build_basic_perceptron(d_train, num, sim_matrix)
 
         score_table[idx + 1][1] = classify_perceptron_set(d_train, bp, 
-          classify_basic_perceptron)
+          classify_basic_perceptron, num)
         score_table[idx + 1][2] = classify_perceptron_set(d_test, bp, 
-          classify_basic_perceptron)
-
+          classify_basic_perceptron, num)
 
     return tabulate(score_table, headers="firstrow")
 
 def main():
-    print perform_tests(data_training_a, data_testing_a)
+    print "\n", perform_tests(data_training_a, data_testing_a)
 
 main()
